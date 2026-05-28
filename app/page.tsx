c706b9668d8721f930a5e2cb84ab1bcfa8c00bfa
Bank1402 <thb1402@hotmail.com>
2026-05-28 21:18:07 +0700

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{
+      padding: "40px",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>SAU LINE SYSTEM</h1>
+      <p>ระบบแนะนำการศึกษาและนักศึกษา</p>
+    </main>
+  );
+}
